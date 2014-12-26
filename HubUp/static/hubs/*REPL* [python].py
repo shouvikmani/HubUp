dcd7ad2ad3910241da433aebb500d580f22833b2
Shouvik Mani <shouvikm@andrew.cmu.edu>
2014-12-26 02:37:59 -0800

    Updated Navbar, Removed Containers, Fixed Carousel Slides, Scrollbar

---
+++ HubUp/static/hubs/*REPL* [python].py
@@ -0,0 +1,5 @@
+  File "/Users/Shouvik/Documents/Shouvik/Computer Science/HubUp2/HubUp/static/hubs/index.css", line 1
+    /*CSS File that contains lots of styling*/
+    ^
+SyntaxError: invalid syntax
+>>> 
